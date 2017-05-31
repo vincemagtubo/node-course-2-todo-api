f9e802094f2e4d83e69fc5a2e0e4e980ddc2f478
vincemagtubo <johnvincemagtubo@gmail.com>
2017-06-01 05:55:47 +0800

    4 Added POST todo routes and refractor mongoose

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -9,17 +9,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
     console.log('Successfully connected to MongoDB server');
 
-    // db.collection('Todos').findOneAndUpdate({
-    //     _id: new ObjectId('592db72859b0961638c5cf88')
-    // }, {
-    //     $set: {
-    //         completed: true
-    //     }
-    // }, {
-    //     returnOriginal: false
-    // }).then((res) => {
-    //     console.log(res);
-    // });
+    db.collection('Todos').findOneAndUpdate({
+        _id: new ObjectId('592db72859b0961638c5cf88')
+    }, {
+        $set: {
+            completed: true
+        }
+    }, {
+        returnOriginal: false
+    }).then((res) => {
+        console.log(res);
+    });
 
     db.collection('Users')
         .findOneAndUpdate({
