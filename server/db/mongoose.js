2baa6e88c5f195bb879935568f05e18feaffef06
vincemagtubo <johnvincemagtubo@gmail.com>
2017-06-07 13:38:09 +0800

    Setup APP for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,22 @@
 var mongoose = require('mongoose');
 
+// //--thought so
+//const{MongoClient, ObjectId} = require('mongodb');
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
+// //--thought so
+// MongoClient.connect('mongodb://vince:password@ds111262.mlab.com:11262/todoapitutorial', (err, db) => {
+//     if (err) {
+//         return console.log('Error happened');
+//     } else {
+//         console.log('Succesfully connected to MongoLab');
+//     }
+// });
 
 module.exports = {
-    mongoose
+    mongoose,
+    // //--thought so--
+    //MongoClient
 };
